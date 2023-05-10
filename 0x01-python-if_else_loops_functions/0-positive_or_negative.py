867312c6cedd59cfab80ba8e5e6fb77e2ec63c52
MaxwellMwirigi03 <maxwellmwithaliii@gmail.com>
2023-05-10 04:06:14 -0700

    Done

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10, 10)
+"""assign a random signed number to the variable number"""
 if number > 0:
-    print(f"{} is positive".format(number))
+    print("{} is positive".format(number))
 elif number == 0:
-    print(f"{} is zero".format(number))
+    print("{} is zero".format(number))
 else:
-    print(f"{} is negative".format(number))
+    print("{} is negative".format(number))
--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-for i in range(97, 123):
-    print(f"{:c}".format(i), end="")
+"""Print the alphabet in lowercase, not followed by a new line."""
+for a in range(97, 123):
+    print("{:c}".format(a), end="")
