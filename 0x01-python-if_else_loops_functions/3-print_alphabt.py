8ee9b4397910b23c35a84d4dc5cbe3f35c4e0207
MaxwellMwirigi03 <maxwellmwithaliii@gmail.com>
2023-05-10 03:35:43 -0700

    Fix

---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+for letr in range(97, 123):
+    if letr != 113 and letr != 101:
+        print("{:c}".format(letr), end="")
--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-#4-print_hexa.py
-for number in range(0, 99):
-    print(f"{} = {}".format(number, hex(number)))
+for num in range(0, 99):
+    print("{:d} = {}".format(num, hex(num)))
